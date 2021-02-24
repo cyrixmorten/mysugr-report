a136612cf185bae63bc5f1bf09d958b0942cb0dc
Morten Rasmussen <cyrixmorten@gmail.com>
2021-02-25 00:22:47 +0100

    refactor: make table using flex layout instead of mat-table

--- libs/mysugr/src/lib/components/table/mysugr-table.component.ts
+++ libs/mysugr/src/lib/components/table/mysugr-table.component.ts
@@ -1,66 +1,44 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {DataSource} from '@angular/cdk/collections';
-import {BehaviorSubject, Observable, Subscription} from 'rxjs';
+import {Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
+import {Subscription} from 'rxjs';
 import {DataHolderService} from "../../services/data-holder.service";
 import {ITimeOfDayGroups, TimeOfDayTransformation} from "../../transform/time.of.day.transformation";
 import {CsvImportService} from "../../services/csv-import.service";
 import {isEmpty} from "lodash-es";
 
 @Component({
-  selector: 'dt-mysugr-table',
-  templateUrl: './mysugr-table.component.html',
-  styleUrls: ['./mysugr-table.component.scss'],
-  encapsulation: ViewEncapsulation.Emulated
+    selector: 'dt-mysugr-table',
+    templateUrl: './mysugr-table.component.html',
+    styleUrls: ['./mysugr-table.component.scss'],
+    encapsulation: ViewEncapsulation.Emulated
 })
-export class MysugrTableComponent implements OnInit {
+export class MysugrTableComponent implements OnInit, OnDestroy {
 
-  displayedColumns: string[] = ['date', 'morning', 'noon', 'evening', 'night'];
-  dataSource: MysugrDataSource;
-  hasData = false;
+    private subscription = new Subscription();
+    private timeOfDayTransformation = new TimeOfDayTransformation();
 
-  constructor(private csvImport: CsvImportService, private dataHolderService: DataHolderService) {
-    this.dataSource = new MysugrDataSource(dataHolderService);
-  }
+    data: ITimeOfDayGroups[];
 
-  ngOnInit(): void {
-    this.csvImport.getResult().subscribe((result) => {
-      if (!isEmpty(result.data)) {
-        this.dataHolderService.setOriginData(result.data);
+    constructor(private csvImport: CsvImportService, private dataHolderService: DataHolderService) {
+    }
 
-        this.hasData = true;
-      }
-    })
-  }
+    ngOnInit(): void {
+        this.subscription.add(
+            this.csvImport.getResult().subscribe((result) => {
+                if (!isEmpty(result.data)) {
+                    this.dataHolderService.setOriginData(result.data);
+                }
+            })
+        )
 
-}
+        this.subscription.add(
+            this.dataHolderService.getData().subscribe((data) => {
+                this.data = this.timeOfDayTransformation.transform(data);
+            })
+        )
+    }
 
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 
-export class MysugrDataSource extends DataSource<ITimeOfDayGroups> {
-
-  private transformed = new BehaviorSubject([]);
-
-  private dataSubscription: Subscription;
-
-  private timeOfDayTransformation = new TimeOfDayTransformation();
-
-  constructor(private dataHolder: DataHolderService) {
-    super();
-  }
-
-  /** Connect function called by the table to retrieve one stream containing the data to render. */
-  connect(): Observable<ITimeOfDayGroups[]> {
-    this.dataSubscription = this.dataHolder.getData().subscribe((data) => {
-      const transformed = this.timeOfDayTransformation.transform(data);
-
-      console.log('transformed', transformed);
-
-      this.transformed.next(transformed);
-    })
-
-    return this.transformed.asObservable();
-  }
-
-  disconnect() {
-    this.dataSubscription.unsubscribe()
-  }
 }
